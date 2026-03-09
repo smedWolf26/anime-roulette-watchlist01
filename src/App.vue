250c1b5a63b365492694adfbf9baaa14b6df67ed
smedWolf26 <smedina.lambda@gmail.com>
2026-03-09 13:42:46 -0500

    Added vueuse and worked on header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex min-h-screen flex-col">
+  <div class="min-h-screen">
     <RouterView />
   </div>
 </template>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,17 @@
 <script setup></script>
-
 <template>
-  <main class="flex flex-1 items-center justify-center">Hello There</main>
+  <main
+    class="min-h-screen bg-[radial-gradient(circle_at_15%_20%,#334155,transparent_40%),radial-gradient(circle_at_85%_0%,#0f766e,transparent_28%),linear-gradient(160deg,#020617,#0f172a,#111827)] px-4 py-8 text-slate-100 sm:px-6 lg:px-8"
+  >
+    <div class="mx-auto max-w-7xl">
+      <header>
+        <p class="text-xs font-semibold tracking-[0.3em] text-blue-400 uppercase">Project #4</p>
+        <h1 class="mt-2 text-4xl font-black text-white sm:text-5xl">Anime Roulette Machine</h1>
+        <p class="mt-2 max-w-3xl text-sm text-slate-300 sm:text-base">
+          Spin the wheel, request a raondom anime from Jinkan with VueUse useFetch, and learn how
+          REST APIs single rate lmiting with HTTP429
+        </p>
+      </header>
+    </div>
+  </main>
 </template>
