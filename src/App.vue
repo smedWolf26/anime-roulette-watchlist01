720c9e05cedf3ba4e4d739c7fc709352362d9adf
smedWolf26 <smedina.lambda@gmail.com>
2026-03-09 09:51:51 -0500

    Cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
-<script setup>
-import NavMenu from '@/components/NavMenu.vue'
-</script>
-
 <template>
   <div class="flex min-h-screen flex-col">
-    <NavMenu />
     <RouterView />
   </div>
 </template>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,5 @@
-<script setup>
-import { ref } from 'vue'
-const title = ref('Hello There ✋')
-</script>
+<script setup></script>
 
 <template>
-  <main class="flex flex-1 items-center justify-center">
-    <h1 class="text-6xl font-thin text-slate-800">{{ title }}</h1>
-  </main>
+  <main class="flex flex-1 items-center justify-center">Hello There</main>
 </template>
